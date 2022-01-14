3768a5a3b2263313e2c072199214fdaabb04eb8b
Woojae Kim <kwj280@gmail.com>
2022-01-14 10:07:09 -0700

    first

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+MapboxGL.setAccessToken('pk.eyJ1Ijoid29vamFlIiwiYSI6ImNreWR5b3UxNTBjMGoyb3NoMjk4eXUxbHcifQ.n4z9_obSqP5bOL6Sq-T5tA');
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: 'tomato'
+  },
+  map: {
+    flex: 1
+  }
 });
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <Text>Mapbox</Text>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    );
+  }
+}
