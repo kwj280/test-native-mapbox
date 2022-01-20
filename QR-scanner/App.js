54f2b5388337c58926e26641705e2073347ea0d3
Woojae Kim <kwj280@gmail.com>
2022-01-19 21:57:04 -0700

    add qr scanner

---
+++ QR-scanner/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, Image, Dimensions} from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  frame: {
+    width: '70%',
+    height: Dimensions.get('window').width * 7/10
+  },
+  scanner: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanning, setScanning] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    console.log(type)
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    setScanning(false)
+  };
+  const onScan = () => {
+    setScanning(true)
+  }
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      {!scanning && <Button
+        onPress={onScan}
+        title="Scan Code"
+        color="#841584"
+        accessibilityLabel="Scan QR code"
+      />}
+
+      {scanning &&
+        <>
+
+          <BarCodeScanner
+            onBarCodeScanned={handleBarCodeScanned}
+            style={styles.scanner}
+          >
+            <Image source={require('./camera_frame.png')} style={styles.frame} />
+          </BarCodeScanner>
+
+        </>
+      }
+    </View>
+  );
+}
