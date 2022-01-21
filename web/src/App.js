87b6b49795ff004aa45dc1566cd2dd110912eea3
Woojae <kwj280@gmail.com>
2022-01-21 03:08:33 -0700

    add qrcode

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Map from './Map'
-
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./Map";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
 function App() {
   return (
     <div className="App">
-      <div style={{height:'80vh'}}>
-      <Map/>
+      <div style={{ height: "80vh" }}>
+        <Map/>
       </div>
+      <QRCode value="http://localhost:5000/api/checkPoint" />,
+
     </div>
   );
 }
-
 
 export default App;
